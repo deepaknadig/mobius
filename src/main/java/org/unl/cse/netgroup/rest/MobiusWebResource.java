b75139f99faaabba374570e5113e61d1e0dca982
Deepak Nadig Anantha <admin@deepaknadig.com>
2017-06-21 11:42:19 -0500

    Added initial /mobius/ndn POST

--- src/main/java/org/unl/cse/netgroup/rest/MobiusWebResource.java
+++ src/main/java/org/unl/cse/netgroup/rest/MobiusWebResource.java
@@ -20,8 +20,12 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.onosproject.rest.AbstractWebResource;
 import org.unl.cse.netgroup.NDNInfo;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
@@ -29,8 +33,11 @@ import java.io.InputStream;
 /**
  * Sample web resource.
  */
-@Path("mobius")
+@Path("")
 public class MobiusWebResource extends AbstractWebResource {
+
+    private static NDNInfo ndnInfo;
+    private final ObjectNode root = mapper().createObjectNode();
 
     /**
      * Get hello world greeting.
@@ -42,6 +49,24 @@ public class MobiusWebResource extends AbstractWebResource {
     public Response getGreeting() {
         ObjectNode node = mapper().createObjectNode().put("hello", "world");
         return ok(node).build();
+    }
+
+    @POST
+    @Path("ndn")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response ndnPostInfo(InputStream stream) throws IOException {
+        // TODO Exception Handling
+//        try {
+//            ObjectNode jsonTree = (ObjectNode) mapper().readTree(stream);
+//        } catch (IOException e) {
+//            throw new IllegalArgumentException(e);
+//        }
+
+        ndnInfo = JsonToNDNInfo(stream);
+        ndnInfo.logInfo();
+
+        return Response.ok(root).build();
     }
 
     private NDNInfo JsonToNDNInfo(InputStream stream) {
